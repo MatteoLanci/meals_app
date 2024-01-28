598f379403eab01f3f8c732da30855957b5f890f
matteo lanci <m.lanci@live.com>
2024-01-28 20:11:09 +0100

    edit in tabs.dart

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod/riverpod.dart';
-// import 'package:meals_app/data/dummy_data.dart';
 import 'package:meals_app/models/meal.dart';
 import 'package:meals_app/screens/categories.dart';
 import 'package:meals_app/screens/filters.dart';
